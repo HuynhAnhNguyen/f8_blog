d745d76cf1b2828118f0af417d9efc614dc02001
HuynhAnhNguyen <nguyenhuynhanh6121@gmail.com>
2023-05-26 21:08:19 +0700

    form default behavior

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 //Template engine
 app.engine('hbs', handlebars.engine({
@@ -16,12 +19,12 @@ app.engine('hbs', handlebars.engine({
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-console.log(path.join(__dirname, 'resources/views'));
+// console.log(path.join(__dirname, 'resources/views'));
 
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
 // Cach 1
 // app.get('/', function(req, res) {
@@ -34,7 +37,17 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+  // console.log(req.query.q);
+  res.render('search');
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send("djedg");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
