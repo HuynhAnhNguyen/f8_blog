e4d1ee1e760e347727f4a6ec6a8d0c617b3fcf9a
HuynhAnhNguyen <nguyenhuynhanh6121@gmail.com>
2023-05-26 21:47:35 +0700

    routes and controllers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars  = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route= require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
   extended: true
@@ -21,33 +23,9 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 // console.log(path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-// Cach 1
-// app.get('/', function(req, res) {
-//     return res.send('Hello World!')
-//   })
-
-// Cach 2
-// app.get('/', (req, res) => res.send('Hello World!'))
+route(app);
 
 
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
-app.get('/search', (req, res) => {
-  // console.log(req.query.q);
-  res.render('search');
-});
-
-app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.send("djedg");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
