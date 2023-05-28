00fe20439c56c915985b18fe02732a09737a05e2
HuynhAnhNguyen <nguyenhuynhanh6121@gmail.com>
2023-05-28 12:13:21 +0700

    install prettier, lint-staged, husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,34 +1,30 @@
-
 const newsRouter = require('./news');
 const siteRouter = require('./site');
 
-function route(app){
-    
-    app.use('/news', newsRouter);
+function route(app) {
+                            app.use('/news', newsRouter);
     app.use('/', siteRouter);
 
     // app.get('/', (req, res) => {
     //     res.render('home');
     // });
-    
+
     // Cach 1
     // app.get('/', function(req, res) {
     //     return res.send('Hello World!')
     //   })
-    
+
     // Cach 2
     // app.get('/', (req, res) => res.send('Hello World!'))
-    
-    
+
     // app.get('/news', (req, res) => {
     //   res.render('news');
     // });
-    
+
     // app.get('/search', (req, res) => {
     //     // console.log(req.query.q);
     //     res.render('search');
     // });
-    
 }
 
-module.exports= route;
+module.exports = route;
